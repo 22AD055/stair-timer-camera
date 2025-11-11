e1b5558ebeabae81a26ab37e0cb0f3dcdabbd71e
22AD055 <22ad055@ms.dendai.ac.jp>
2025-11-12 01:16:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function onScanSuccess(decodedText) {
 
     const name = prompt("名前を入力してください（ランキング用）:");
     if (name) {
-      fetch("https://script.google.com/macros/s/AKfycbw9FVGpjw3tKr-8gtuVFQexVVFnEBw5b_D6cvAIcEu3nAnJ_AtkfihvM6LkuHwUU7QJ1Q/exec", {
+      fetch("https://script.google.com/macros/s/AKfycbyh0pzoZdrvG6km45MXm4wMl5a8PSwfYlZADaHhGBQFf9BdWgpdyYkTpHpdUmQSQp9Hlg/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name: name, time: elapsed })

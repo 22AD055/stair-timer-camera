0c458522864e560e2bb4997010c976ed791fd50e
22AD055 <22ad055@ms.dendai.ac.jp>
2025-11-13 00:50:08 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -52,11 +52,11 @@ function onScanSuccess(decodedText) {
   // STOP_QRを読み取った場合
   else if (stage === "running" && decodedText === "STOP_QR") {
     stage = "finished";
-    const elapsed = ((Date.now() - startTime) / 1000).toFixed(2);
-    document.getElementById("status").textContent = `結果：${elapsed} 秒！`;
-    document.getElementById("timer").textContent = `${elapsed} 秒`;
+const elapsed = ((Date.now() - startTime) / 1000).toFixed(2);
+document.getElementById("status").innerHTML = `<span style="font-size:2.5em; font-weight:bold;">結果：${elapsed} 秒！</span>`;
+    document.getElementById("timer").textContent = `${elapsed} 秒あ`;
 
-    const name = prompt("名前を入力してください（ランキング用）:");
+    const name = prompt("名前を入力してください（ランキング用）");
     if (name) {
       sendToGoogleForm(name, elapsed);
     }
